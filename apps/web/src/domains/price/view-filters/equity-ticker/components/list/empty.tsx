529b317fb1e85f679862ba1ef03f2eddd393306e
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-11-13 21:35:53 +0700

    feat(web): empty ticker filter list state

--- apps/web/src/domains/price/view-filters/equity-ticker/components/list/empty.tsx
+++ apps/web/src/domains/price/view-filters/equity-ticker/components/list/empty.tsx
@@ -1,3 +1,18 @@
+import emptyBox from "-/assets/empty-box.png";
+import { Card, CardContent } from "-/components/ui/card";
+
 export const EquityTickerListEmpty = () => {
-  return "No data found";
+  return (
+    <Card>
+      <CardContent className="min-h-24 place-items-center space-y-2">
+        <img className="pointer-events-none size-20 grayscale" src={emptyBox} />
+        <div className="flex flex-col gap-1.5 text-center text-balance">
+          <strong>No data found</strong>
+          <p className="text-muted-foreground text-xs">
+            We can&apos;t find the data you're looking for...
+          </p>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };

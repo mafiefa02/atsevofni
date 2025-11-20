28b9ad6c4ee503d1b72269846d8491c8742cd4d6
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-11-20 12:48:06 +0700

    feat(web): adjust loading state to minimize layout shift

--- apps/web/src/domains/price/views/chart-vis/components/chart/loading.tsx
+++ apps/web/src/domains/price/views/chart-vis/components/chart/loading.tsx
@@ -4,11 +4,11 @@ import { Skeleton } from "-/components/ui/skeleton";
 export const LoadingPriceChart = () => {
   return (
     <Card className="overflow-hidden">
-      <CardContent className="h-[28rem] w-full">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <Skeleton className="h-6 w-52" />
-          <Skeleton className="h-72 w-full" />
-          <Skeleton className="h-28 w-full" />
+      <CardContent className="h-[25rem] w-full">
+        <div className="flex h-full flex-col items-center justify-center gap-2">
+          <Skeleton className="h-8 w-52" />
+          <Skeleton className="h-full w-full" />
+          <Skeleton className="h-12 w-full" />
         </div>
       </CardContent>
     </Card>
